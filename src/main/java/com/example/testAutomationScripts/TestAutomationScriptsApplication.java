aaf578eadb017991ddf88cf1a25cbc1d97744202
ernest-emmanuel-utibe <em123nest@gmail.com>
2024-07-02 12:51:26 +0100

    Initial Commit

---
+++ src/main/java/com/example/testAutomationScripts/TestAutomationScriptsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testAutomationScripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutomationScriptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAutomationScriptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testAutomationScripts/TestAutomationScriptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testAutomationScripts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutomationScriptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
